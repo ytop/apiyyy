8456671a07d09b335da1fbcb3e41205ec65d3486
ytop <doghasbaby@gmail.com>
2026-01-14 11:25:41 -0500

    add readme

--- chatterbox/helloworld.py
+++ chatterbox/helloworld.py
@@ -1,9 +1,3 @@
-# apiyyy
-It is an API service which can get the request of text and sample wav file(Optional) , then convert text to wav file.
-
-The service sample code is like,
-
-`
 import torch
 import torchaudio as ta
 from chatterbox.tts import ChatterboxTTS
@@ -24,13 +18,3 @@ text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the e
 wav = model.generate(text, language_id="en")
 ta.save("test-english.wav", wav, model.sr)
 
-# If you want to synthesize with a different voice, specify the audio prompt
-AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
-wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
-ta.save("test-2.wav", wav, model.sr)
-`
-
-
-
-# technical stack
-fastapi, installed already
